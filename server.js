7641b4f5ee7f4723b3e84f1ae26ef5fae382132d
Faran <120464126+faran151@users.noreply.github.com>
2025-09-29 20:59:13 +0500

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('My First Containerized Node App');
+  res.send('App on Azure ');
 });
 
 app.listen(PORT, HOST, () => {
